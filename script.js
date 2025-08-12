4190ff0a2530ea5661320f55f479d44f27f7e924
Naifi <makruf195404@gmail.com>
2025-08-12 07:31:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-// Animasi fade-in saat scroll
+// Fade-in scroll animation dengan delay
 const fadeElements = document.querySelectorAll('.fade-in');
 
 function checkScroll() {
-    fadeElements.forEach(el => {
+    fadeElements.forEach((el, i) => {
         const rect = el.getBoundingClientRect();
-        if (rect.top < window.innerHeight - 100) {
-            el.classList.add('show');
+        if (rect.top < window.innerHeight - 80) {
+            setTimeout(() => {
+                el.classList.add('show');
+            }, i * 150); // delay antar elemen
         }
     });
 }
